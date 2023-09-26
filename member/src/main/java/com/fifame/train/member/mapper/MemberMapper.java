78ec331124e9966c20e82491eaf04401aa979726
fifame <fifame@qq.com>
2023-09-26 17:53:21 +0800

    20230926_17:52:增加数据库连接,并增加mybatis

---
+++ member/src/main/java/com/fifame/train/member/mapper/MemberMapper.java
@@ -0,0 +1,12 @@
+package com.fifame.train.member.mapper;
+
+/**
+ * @InterfaceName MemberMapper
+ * @Description TODO
+ * @Author fifame
+ * @DATE 2023-09-26 17:15
+ * @Version 1.0
+ */
+public interface MemberMapper {
+    int count();
+}
