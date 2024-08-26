18522b6b85629216bc1b9d180f63d8db17514fed
fifame <1342547041@qq.com>
2024-08-26 15:25:07 +0800

    20230926_21:25:增加统一返回结果

---
+++ common/src/main/java/com/fifame/train/common/exception/BusinessException.java
@@ -0,0 +1,12 @@
+package com.fifame.train.common.exception;
+
+/**
+ * @ClassName BusinessException
+ * @Description TODO
+ * @Author fifame
+ * @DATE 2023-09-27 8:29
+ * @Version 1.0
+ */
+public enum BusinessException {
+}
+
